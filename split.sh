039b494bf7aaab56bc5a3c3bc376c0221b83bac1
mmoreram <marc.morera@befactory.es>
2014-04-07 19:39:44 +0200

    Issue #5 split file added
    * This file automatizes the components update process

---
+++ split.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+for i in $(ls -1 src/Elcodi/); do
+    rm -rf elcodi.$i
+    git clone git@github.com:elcodi/elcodi.git elcodi.$i;
+    cd elcodi.$i;
+    git filter-branch --prune-empty --subdirectory-filter src/Elcodi/$i;
+    git remote rm origin
+    git remote add origin git@github.com:elcodi/$i.git
+    git push origin master
+    git push --tags
+    cd ..
+    rm -rf elcodi.$i
+done
