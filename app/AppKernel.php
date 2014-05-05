e83ad274dc62b7d576a81472d44e14dd2d6f76a2
mmoreram <marc.morera@befactory.es>
2014-05-05 17:50:34 +0200

    Issue #24 Formatted all fileblocks

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,8 +1,23 @@
 <?php
 
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author ##author_placeholder
+ * @version ##version_placeholder##
+ */
+
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 
+/**
+ * Class AppKernel
+ */
 class AppKernel extends Kernel
 {
     public function registerBundles()
