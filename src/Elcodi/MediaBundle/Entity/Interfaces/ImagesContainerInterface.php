a048d899ac37d0aac30ffe9213f2df26ae15e69c
mmoreram <marc.morera@befactory.es>
2014-05-06 18:36:28 +0200

    Issue #24
    * Added ImageContainerTrait
    * Changed image table name
    * Created ImageContainerInterface

---
+++ src/Elcodi/MediaBundle/Entity/Interfaces/ImagesContainerInterface.php
@@ -0,0 +1,74 @@
+<?php
+
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author  * @version
+ */
+
+namespace Elcodi\MediaBundle\Entity\Interfaces;
+
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * Class ImagesContainerInterface
+ */
+interface ImagesContainerInterface
+{
+    /**
+     * Set add image
+     *
+     * @param ImageInterface $image Image object to be added
+     *
+     * @return Object self Object
+     */
+    public function addImage(ImageInterface $image);
+
+    /**
+     * Get if entity is enabled
+     *
+     * @param ImageInterface $image Image object to be removed
+     *
+     * @return Object self Object
+     */
+    public function removeImage(ImageInterface $image);
+
+    /**
+     * Get all images
+     *
+     * @return ArrayCollection
+     */
+    public function getImages();
+
+    /**
+     * Set images
+     *
+     * @param ArrayCollection $images Images
+     *
+     * @return Object self Object
+     */
+    public function setImages(ArrayCollection $images);
+
+    /**
+     * Set the principalImage
+     *
+     * @param ImageInterface $principalImage Principal image
+     *
+     * @return ImagesContainerInterface self Object
+     */
+    public function setPrincipalImage(ImageInterface $principalImage = null);
+
+
+    /**
+     * Get the principalImage
+     *
+     * @return ImageInterface Principal image
+     */
+    public function getPrincipalImage();
+}
+ 
---
+++ src/Elcodi/MediaBundle/Entity/Traits/ImagesContainerTrait.php
@@ -0,0 +1,121 @@
+<?php
+
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author ##author_placeholder
+ * @version ##version_placeholder##
+ */
+ 
+namespace Elcodi\MediaBundle\Entity\Traits;
+
+/**
+ * Trait ImagesContainerTrait
+ */
+trait ImagesContainerTrait
+{
+    /**
+     * @var \Doctrine\Common\Collections\Collection
+     *
+     * Images
+     */
+    protected $images;
+
+    /**
+     * @var \Elcodi\MediaBundle\Entity\Interfaces\ImageInterface
+     *
+     * Principal image
+     */
+    protected $principalImage;
+
+    /**
+     * Set add image
+     *
+     * @param \Elcodi\MediaBundle\Entity\Interfaces\ImageInterface $image Image object to be added
+     *
+     * @return Object self Object
+     */
+    public function addImage(\Elcodi\MediaBundle\Entity\Interfaces\ImageInterface $image)
+    {
+        $this->images->add($image);
+
+        if (empty($this->principalImage)) {
+
+            $this->setPrincipalImage($image);
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * Get if entity is enabled
+     *
+     * @param \Elcodi\MediaBundle\Entity\Interfaces\ImageInterface $image Image object to be removed
+     *
+     * @return Object self Object
+     */
+    public function removeImage(\Elcodi\MediaBundle\Entity\Interfaces\ImageInterface $image)
+    {
+        $this->images->removeElement($image);
+
+        return $this;
+    }
+
+
+    /**
+     * Get all images
+     *
+     * @return \Doctrine\Common\Collections\ArrayCollection
+     */
+    public function getImages()
+    {
+        return $this->images;
+    }
+
+
+    /**
+     * Set images
+     *
+     * @param \Doctrine\Common\Collections\ArrayCollection $images Images
+     *
+     * @return Object self Object
+     */
+    public function setImages(\Doctrine\Common\Collections\ArrayCollection $images)
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+
+    /**
+     * Set the principalImage
+     *
+     * @param \Elcodi\MediaBundle\Entity\Interfaces\ImageInterface $principalImage Principal image
+     *
+     * @return Object self Object
+     */
+    public function setPrincipalImage(\Elcodi\MediaBundle\Entity\Interfaces\ImageInterface $principalImage = null)
+    {
+        $this->principalImage = $principalImage;
+
+        return $this;
+    }
+
+
+    /**
+     * Get the principalImage
+     *
+     * @return \Elcodi\MediaBundle\Entity\Interfaces\ImageInterface Principal image
+     */
+    public function getPrincipalImage()
+    {
+        return $this->principalImage;
+    }
+}
+ 
