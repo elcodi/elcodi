d7b2d31ae47c080e1c4839df57882be1ca9144f7
mmoreram <marc.morera@befactory.es>
2014-05-14 20:19:59 +0200

    Fixed all wrong formatted file phpdoc

--- src/Elcodi/CartCouponBundle/Event/CouponCartPreApplyEvent.php
+++ src/Elcodi/CartCouponBundle/Event/CouponCartPreApplyEvent.php
@@ -8,7 +8,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author  * @version
+ * @author ##author_placeholder
+ * @version ##version_placeholder##
  */
 
 namespace Elcodi\CartCouponBundle\Event;
