cf96226b6de9102854632d2f162091cfeabe9fc2
mmoreram <marc.morera@befactory.es>
2014-06-12 18:29:12 +0200

    wip

--- src/Elcodi/CurrencyBundle/Tests/UnitTest/ValueObject/MoneyTest.php
+++ src/Elcodi/CurrencyBundle/Tests/UnitTest/ValueObject/MoneyTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * This file is part of the Elcodi package.
  *
@@ -7,7 +8,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * @author ##author_placeholder
+ * @author  ##author_placeholder
  * @version ##version_placeholder##
  */
 
