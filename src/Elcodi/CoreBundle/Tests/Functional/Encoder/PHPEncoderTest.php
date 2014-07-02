256fee144718471317dd8d6588b67ce99e23dfc9
mmoreram <marc.morera@befactory.es>
2014-07-02 11:45:39 +0200

    Issue #112

---
+++ src/Elcodi/CoreBundle/Tests/Functional/Encoder/PHPEncoderTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\CoreBundle\Tests\Functional\Encoder;
+
+use Elcodi\CoreBundle\Tests\WebTestCase;
+
+/**
+ * Class PHPEncoderTest
+ */
+class PHPEncoderTest extends WebTestCase
+{
+    /**
+     * Returns the callable name of the service
+     *
+     * @return string service name
+     */
+    public function getServiceCallableName()
+    {
+        return [
+            'elcodi.core.core.encoder.php',
+            'elcodi.php_encoder',
+        ];
+    }
+}
