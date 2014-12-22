48dc2a59410188cee2977e03c17d30a0d3627200
Berny Cantos <xphere@elcodi.com>
2014-12-22 19:28:34 +0100

    Add PluginBundle

---
+++ src/Elcodi/Bundle/PluginBundle/DependencyInjection/ElcodiPluginExtension.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Bundle\PluginBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+
+use Elcodi\Bundle\CoreBundle\DependencyInjection\Abstracts\AbstractExtension;
+
+/**
+ * Class ElcodiPluginExtension
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+class ElcodiPluginExtension extends AbstractExtension
+{
+    /**
+     * @var string
+     *
+     * Extension name
+     */
+    const EXTENSION_NAME = 'elcodi_plugin';
+
+    /**
+     * Get the Config file location
+     *
+     * @return string Config file location
+     */
+    public function getConfigFilesLocation()
+    {
+        return __DIR__ . '/../Resources/config';
+    }
+
+    /**
+     * Config files to load
+     *
+     * @param array $config Configuration
+     *
+     * @return array Config files
+     */
+    public function getConfigFiles(array $config)
+    {
+        return [
+            'classes',
+            'services',
+            'twig',
+        ];
+    }
+
+    /**
+     * Return a new Configuration instance.
+     *
+     * If object returned by this method is an instance of
+     * ConfigurationInterface, extension will use the Configuration to read all
+     * bundle config definitions.
+     *
+     * Also will call getParametrizationValues method to load some config values
+     * to internal parameters.
+     *
+     * @return ConfigurationInterface Configuration file
+     */
+    protected function getConfigurationInstance()
+    {
+        return new Configuration($this->getAlias());
+    }
+
+    /**
+     * Hook after load the full container.
+     *
+     * @param array            $config
+     * @param ContainerBuilder $container
+     */
+    public function postLoad(array $config, ContainerBuilder $container)
+    {
+        parent::postLoad($config, $container);
+
+        if (!$container->has($config['hook_system'])) {
+
+            throw new InvalidArgumentException(sprintf(
+                'The config "%s.hook_system" has a dependency on a non-existent service "%s".',
+                $this->getAlias(),
+                $config['hook_system']
+            ));
+        }
+
+        $container->setAlias('elcodi.hook_system', $config['hook_system']);
+    }
+
+    /**
+     * Returns the extension alias, same value as extension name
+     *
+     * @return string The alias
+     */
+    public function getAlias()
+    {
+        return self::EXTENSION_NAME;
+    }
+}
---
+++ src/Elcodi/Bundle/PluginBundle/Twig/HookExtension.php
@@ -0,0 +1,72 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Bundle\PluginBundle\Twig;
+
+use Elcodi\Component\Plugin\HookSystemInterface;
+
+/**
+ * Class HookExtension for Twig
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+class HookExtension extends \Twig_Extension
+{
+    /**
+     * @var HookSystemInterface $hookSystem
+     *
+     * Where to execute the hooks
+     */
+    protected $hookSystem;
+
+    /**
+     * Construct
+     *
+     * @param HookSystemInterface $hookSystem
+     */
+    public function __construct(HookSystemInterface $hookSystem)
+    {
+        $this->hookSystem = $hookSystem;
+    }
+
+    /**
+     * Returns a list of functions to add to the existing list.
+     *
+     * @return array An array of functions
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction(
+                'elcodi_hook',
+                [$this->hookSystem, 'execute'],
+                [
+                    'is_safe' => ['html'],
+                ]
+            ),
+        ];
+    }
+
+    /**
+     * Returns the name of the extension.
+     *
+     * @return string The extension name
+     */
+    public function getName()
+    {
+        return 'hook_extension';
+    }
+}
