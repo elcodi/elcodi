719979929486b88620256f4c4b1c3e1c04d252b3
Berny Cantos <xphere@elcodi.com>
2014-12-22 19:23:51 +0100

    Adapter for EventDispatcher

---
+++ src/Elcodi/Component/Plugin/Adapter/EventDispatcher/EventAdapter.php
@@ -0,0 +1,106 @@
+<?php
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\Plugin\Adapter\EventDispatcher;
+
+use Symfony\Component\EventDispatcher\Event;
+
+use Elcodi\Component\Plugin\EventInterface;
+
+/**
+ * Class EventAdapter
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+class EventAdapter extends Event implements EventInterface
+{
+    /**
+     * @var array
+     *
+     * Context from the caller
+     */
+    protected $context;
+
+    /**
+     * @var string
+     *
+     * Content to be returned
+     */
+    protected $content;
+
+    /**
+     * Create a new event from context and content
+     *
+     * @param array  $context Caller's context
+     * @param string $content Original content
+     */
+    public function __construct(array $context = array(), $content = '')
+    {
+        $this->content = $content;
+        $this->context = $context;
+    }
+
+    /**
+     * Get a value from the context, with a fallback default
+     *
+     * @param string     $key     Index in the context
+     * @param mixed|null $default Default value if there's no entry
+     *
+     * @return mixed
+     */
+    public function get($key, $default = null)
+    {
+        if (array_key_exists($key, $this->context)) {
+
+            return $this->context[$key];
+        }
+
+        return $default;
+    }
+
+    /**
+     * Get full context
+     *
+     * @return array
+     */
+    public function getContext()
+    {
+        return $this->context;
+    }
+
+    /**
+     * Get current content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return (string) $this->content;
+    }
+
+    /**
+     * Set current content
+     *
+     * @param  string $content
+     *
+     * @return $this self Object
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+}
