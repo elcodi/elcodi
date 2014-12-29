c546351263018fb5c714d3c3a19002fd8e1af19a
Berny Cantos <xphere@elcodi.com>
2014-12-29 18:59:03 +0100

    bundle skeleton

---
+++ src/Elcodi/Plugin/ProductCsv/DependencyInjection/ProductCsvExtension.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Plugin\ProductCsv\DependencyInjection;
+
+use Elcodi\Bundle\CoreBundle\DependencyInjection\Abstracts\AbstractExtension;
+
+/**
+ * Class ProductCsvExtension
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+class ProductCsvExtension extends AbstractExtension
+{
+    /**
+     * Returns the recommended alias to use in XML.
+     *
+     * This alias is also the mandatory prefix to use when using YAML.
+     *
+     * @return string The alias
+     */
+    public function getAlias()
+    {
+        return 'plugin_product_csv';
+    }
+
+    /**
+     * Get the Config file location
+     *
+     * @return string Config file location
+     */
+    protected function getConfigFilesLocation()
+    {
+        return __DIR__ . '/../Resources/config';
+    }
+
+    /**
+     * Config files to load
+     *
+     * Each array position can be a simple file name if must be loaded always,
+     * or an array, with the filename in the first position, and a boolean in
+     * the second one.
+     *
+     * As a parameter, this method receives all loaded configuration, to allow
+     * setting this boolean value from a configuration value.
+     *
+     * return array(
+     *      'file1.yml',
+     *      'file2.yml',
+     *      ['file3.yml', $config['my_boolean'],
+     *      ...
+     * );
+     *
+     * @param array $config Config definitions
+     *
+     * @return array Config files
+     */
+    protected function getConfigFiles(array $config)
+    {
+        return [
+            'classes',
+            'services',
+        ];
+    }
+}
