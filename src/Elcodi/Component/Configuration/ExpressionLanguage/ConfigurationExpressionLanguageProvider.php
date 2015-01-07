6a25d49971f4cd5ffdaa0d4ed2e7a893feda97f0
Berny Cantos <xphere@elcodi.com>
2015-01-07 10:43:39 +0100

    add expression language provider

---
+++ src/Elcodi/Component/Configuration/ExpressionLanguage/ConfigurationExpressionLanguageProvider.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\Configuration\ExpressionLanguage;
+
+use Symfony\Component\ExpressionLanguage\ExpressionFunction;
+use Symfony\Component\ExpressionLanguage\ExpressionFunctionProviderInterface;
+
+class ConfigurationExpressionLanguageProvider implements ExpressionFunctionProviderInterface
+{
+    public function getFunctions()
+    {
+        return array(
+            new ExpressionFunction('elcodi_config', function ($name) {
+                    return sprintf('$this->get(\'elcodi.configuration_manager\')->getParameter(%s)', $name);
+                }, function (array $variables, $name) {
+                    return $variables['container']->get('elcodi.configuration_manager')->getParameter($name);
+                }),
+        );
+    }
+}
