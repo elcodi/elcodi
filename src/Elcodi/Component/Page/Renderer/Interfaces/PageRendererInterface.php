3b99f3ecc80aecdc0062ba14279e5555018c40ce
Berny Cantos <xphere@elcodi.com>
2015-01-14 15:39:16 +0100

    add support for PageRenderers

---
+++ src/Elcodi/Component/Page/Renderer/Interfaces/PageRendererInterface.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Component\Page\Renderer\Interfaces;
+
+use Elcodi\Component\Page\Entity\Interfaces\PageInterface;
+
+/**
+ * Interface PageRendererInterface
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+interface PageRendererInterface
+{
+    /**
+     * Render a page
+     *
+     * @param PageInterface $page Page to render
+     *
+     * @return string Rendered content
+     */
+    public function render(PageInterface $page);
+
+    /**
+     * Check for render support of a page
+     *
+     * @param PageInterface $page Page to check support
+     *
+     * @return bool
+     */
+    public function supports(PageInterface $page);
+}
