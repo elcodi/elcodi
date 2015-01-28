a023e327e1b7838def06f7cf7446dfeb14473887
mmoreram <marc.morera@befactory.es>
2015-01-28 13:36:53 +0100

    Added empty Google Analytics plugin

---
+++ src/Elcodi/Plugin/GoogleAnalyticsBundle/ElcodiGoogleAnalyticsBundle.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Plugin\GoogleAnalyticsBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+use Elcodi\Bundle\PluginBundle\Interfaces\PluginBundleInterface;
+
+/**
+ * Class ElcodiGoogleAnalyticsBundle
+ */
+class ElcodiGoogleAnalyticsBundle extends Bundle implements PluginBundleInterface
+{
+}
