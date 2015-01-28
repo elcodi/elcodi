48cec0d955bf8e404ef00aa5a0dbe5545bbf6d79
mmoreram <marc.morera@befactory.es>
2015-01-28 13:33:21 +0100

    Added Pinterest plugin
    * Created plugin
    * Added event listeners for twig events
    * Created administration panel for configuration

---
+++ src/Elcodi/Plugin/PinterestBundle/Controller/AdminController.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+namespace Elcodi\Plugin\PinterestBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Request;
+
+use Elcodi\Admin\CoreBundle\Controller\Abstracts\AbstractAdminController;
+use Elcodi\Component\Plugin\Entity\Plugin;
+
+/**
+ * Class AdminController
+ */
+class AdminController extends AbstractAdminController
+{
+    /**
+     * @Route(
+     *      path = "/pinterest",
+     *      name = "admin_pinterest_configuration",
+     *      methods = {"GET", "POST"}
+     * )
+     * @Template()
+     */
+    public function configurationAction(Request $request)
+    {
+        /**
+         * @var Plugin $plugin
+         */
+        $plugin = $this
+            ->get('elcodi.plugin_manager')
+            ->getPlugin('Elcodi\Plugin\PinterestBundle');
+
+        if ('POST' === $request->getMethod()) {
+
+            $this
+                ->get('elcodi.plugin_manager')
+                ->updatePlugin(
+                    'Elcodi\Plugin\PinterestBundle',
+                    $plugin->isEnabled(),
+                    [
+                        'asynchronous' => (boolean) $request->request->get('asynchronous')
+                    ]
+                );
+
+            return $this->redirectToRoute('admin_pinterest_configuration');
+        }
+
+        return [
+            'plugin' => $plugin,
+        ];
+    }
+}
