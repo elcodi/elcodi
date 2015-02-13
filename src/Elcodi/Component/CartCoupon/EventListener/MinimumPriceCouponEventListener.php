ba1265f2f4b806213c877d9de9e3ec8c96dc6882
Berny Cantos <xphere@elcodi.com>
2015-02-13 16:31:33 +0100

    check minimum price on coupon apply

---
+++ src/Elcodi/Component/CartCoupon/EventListener/MinimumPriceCouponEventListener.php
@@ -0,0 +1,79 @@
+<?php
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+
+namespace Elcodi\Component\CartCoupon\EventListener;
+
+use Elcodi\Component\CartCoupon\Event\CartCouponOnApplyEvent;
+use Elcodi\Component\Coupon\Exception\CouponBelowMinimumPurchaseException;
+use Elcodi\Component\Currency\Entity\NullMoney;
+use Elcodi\Component\Currency\Services\CurrencyConverter;
+
+/**
+ * Class MinimumPriceCouponEventListener
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+class MinimumPriceCouponEventListener
+{
+    /**
+     * @var CurrencyConverter
+     */
+    protected $currencyConverter;
+
+    /**
+     * @param $currencyConverter
+     */
+    public function __construct($currencyConverter)
+    {
+        $this->currencyConverter = $currencyConverter;
+    }
+
+    /**
+     * Check if cart meets minimum price requirements for a coupon
+     *
+     * @param CartCouponOnApplyEvent $event
+     *
+     * @throws CouponBelowMinimumPurchaseException
+     */
+    public function checkMinimumPrice(CartCouponOnApplyEvent $event)
+    {
+        $couponMinimumPrice = $event->getCoupon()->getMinimumPurchase();
+        if ($couponMinimumPrice->getAmount() === 0) {
+
+            return;
+        }
+
+        $productMoney = $event->getCart()->getProductAmount();
+        if ($productMoney->getAmount() === 0) {
+
+            return;
+        }
+
+        if ($couponMinimumPrice->getCurrency() != $productMoney->getCurrency()) {
+            $couponMinimumPrice = $this
+                ->currencyConverter
+                ->convertMoney(
+                    $couponMinimumPrice,
+                    $productMoney->getCurrency()
+                );
+        }
+
+        if ($productMoney->isLessThan($couponMinimumPrice)) {
+
+            throw new CouponBelowMinimumPurchaseException();
+        }
+    }
+}
