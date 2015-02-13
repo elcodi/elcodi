8c0c699f24d48683075c275292fb2360c641b49b
Berny Cantos <xphere@elcodi.com>
2015-02-13 16:31:14 +0100

    check coupon validity on apply

---
+++ src/Elcodi/Component/CartCoupon/EventListener/CheckCouponEventListener.php
@@ -0,0 +1,58 @@
+<?php
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014 Elcodi.com
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ */
+
+
+namespace Elcodi\Component\CartCoupon\EventListener;
+
+use Elcodi\Component\CartCoupon\Event\CartCouponOnApplyEvent;
+use Elcodi\Component\Coupon\Exception\Abstracts\AbstractCouponException;
+use Elcodi\Component\Coupon\Services\CouponManager;
+
+/**
+ * Class CheckCouponEventListener
+ *
+ * @author Berny Cantos <be@rny.cc>
+ */
+class CheckCouponEventListener
+{
+    /**
+     * @var CouponManager
+     */
+    protected $couponManager;
+
+    /**
+     * Constructor
+     *
+     * @param CouponManager $couponManager
+     */
+    public function __construct(CouponManager $couponManager)
+    {
+        $this->couponManager = $couponManager;
+    }
+
+    /**
+     * Check if cart meets basic requirements for a coupon
+     *
+     * @param CartCouponOnApplyEvent $event
+     *
+     * @throws AbstractCouponException
+     */
+    public function checkCoupon(CartCouponOnApplyEvent $event)
+    {
+        $coupon = $event->getCoupon();
+
+        $this->couponManager->checkCoupon($coupon);
+    }
+}
