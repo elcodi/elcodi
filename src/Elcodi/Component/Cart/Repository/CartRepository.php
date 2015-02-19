f94812c6bb5bb310a9b89299af6f67b3624bf07a
Roger Gros <regor1985@gmail.com>
2015-02-19 11:57:38 +0100

    Cart is updated if it's address changes

--- src/Elcodi/Component/Cart/Repository/CartRepository.php
+++ src/Elcodi/Component/Cart/Repository/CartRepository.php
@@ -18,10 +18,33 @@
 namespace Elcodi\Component\Cart\Repository;
 
 use Doctrine\ORM\EntityRepository;
+use Elcodi\Component\Geo\Entity\Interfaces\AddressInterface;
 
 /**
  * CartRepository
  */
 class CartRepository extends EntityRepository
 {
+    public function findAllCartsWithAddress(AddressInterface $address)
+    {
+        $queryBuilder = $this
+            ->createQueryBuilder('c');
+
+        $result = $queryBuilder
+            ->innerJoin('c.billingAddress', 'b')
+            ->innerJoin('c.deliveryAddress', 'd')
+            ->where(
+                $queryBuilder->expr()->orX(
+                    $queryBuilder->expr()->eq('b.id', ':customerId'),
+                    $queryBuilder->expr()->eq('c.id', ':addressId')
+                )
+            )
+            ->setParameter('customerId', $address->getId())
+            ->setParameter('addressId', $address->getId())
+            ->getQuery()
+            ->getResult();
+
+        return $result;
+    }
+
 }
