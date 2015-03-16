cb5546d80c5215526012e31f961dbd5e01962294
Philipp Wahala <philipp.wahala@gmail.com>
2015-03-16 06:28:18 +0100

    Add CurrencyConverter typehint to MinimumPriceCouponListener constructor

--- src/Elcodi/Component/CartCoupon/EventListener/MinimumPriceCouponListener.php
+++ src/Elcodi/Component/CartCoupon/EventListener/MinimumPriceCouponListener.php
@@ -36,7 +36,7 @@ class MinimumPriceCouponListener
     /**
      * @param $currencyConverter
      */
-    public function __construct($currencyConverter)
+    public function __construct(CurrencyConverter $currencyConverter)
     {
         $this->currencyConverter = $currencyConverter;
     }
