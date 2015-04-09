c8dcfb00873debb3b6434762680c0fae7c9b3043
Luis Cordova <cordoval@gmail.com>
2015-04-09 03:40:51 -0500

    rename service from elcodi.cart.manager to elcodi.manager.cart

--- src/Elcodi/Bundle/CartBundle/Tests/Functional/Services/CartManagerVariantTest.php
+++ src/Elcodi/Bundle/CartBundle/Tests/Functional/Services/CartManagerVariantTest.php
@@ -144,11 +144,11 @@ class CartManagerVariantTest extends AbstractCartManagerTest
     public function testAddSameVariantTwice()
     {
         $this
-            ->get('elcodi.cart.manager')
+            ->get('elcodi.manager.cart')
             ->addProduct($this->cart, $this->purchasable, 1);
 
         $this
-            ->get('elcodi.cart.manager')
+            ->get('elcodi.manager.cart')
             ->addProduct($this->cart, $this->purchasable, 2);
 
         $this->assertEquals(1, $this->cart->getCartLines()->count());
