18c9a7438c2816667dcb88d0dcf2b1b9613d6fa2
mmoreram <marc.morera@befactory.es>
2015-06-07 16:50:02 +0200

    Stabilization of all dependencies
    * Updated the dependency with Symfony to 2.7 LTS
    * Changed from `~` to `^` in order to avoid complex dependency definitions
    * Stabilized prefer-lowest as well with Sf2.7

--- src/Elcodi/Bundle/TestCommonBundle/Functional/WebTestCase.php
+++ src/Elcodi/Bundle/TestCommonBundle/Functional/WebTestCase.php
@@ -61,6 +61,7 @@ abstract class WebTestCase extends BaseWebTestCase
             static::$application->setAutoExit(false);
             $this->container = static::$kernel->getContainer();
         } catch (Exception $e) {
+            var_dump($e->getTrace());
             throw new RuntimeException(
                 sprintf('Unable to start the application: %s', $e->getMessage()),
                 $e->getCode(),
