0b32f54ca086824e1da80322c18289c632274831
Berny Cantos <xphere@elcodi.com>
2015-09-08 19:00:26 +0200

    can disable entities

--- src/Elcodi/Bundle/CoreBundle/DependencyInjection/Abstracts/AbstractConfiguration.php
+++ src/Elcodi/Bundle/CoreBundle/DependencyInjection/Abstracts/AbstractConfiguration.php
@@ -86,6 +86,9 @@ abstract class AbstractConfiguration implements ConfigurationInterface
         $node = $builder->root($nodeName);
 
         $node
+            ->treatFalseLike([
+                'enabled' => false,
+            ])
             ->addDefaultsIfNotSet()
             ->children()
                 ->scalarNode('class')
@@ -102,7 +105,6 @@ abstract class AbstractConfiguration implements ConfigurationInterface
                 ->end()
                 ->booleanNode('enabled')
                     ->defaultValue($entityEnabled)
-                    ->cannotBeEmpty()
                 ->end()
             ->end()
         ->end();
