03a2a3d6c08406525040f96e3bcdd1bf029f2eda
Emanuele Minotto <minottoemanuele@gmail.com>
2015-12-08 01:07:32 +0100

    Implemented tests for Tax component

---
+++ src/Elcodi/Component/Tax/Tests/UnitTest/Entity/TaxTest.php
@@ -0,0 +1,103 @@
+<?php
+
+/*
+ * This file is part of the Elcodi package.
+ *
+ * Copyright (c) 2014-2015 Elcodi Networks S.L.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Feel free to edit as you please, and have fun.
+ *
+ * @author Marc Morera <yuhu@mmoreram.com>
+ * @author Aldo Chiecchia <zimage@tiscali.it>
+ * @author Elcodi Team <tech@elcodi.com>
+ */
+
+namespace Elcodi\Component\Tax\Tests\UnitTest\Entity;
+
+use Elcodi\Component\Core\Tests\UnitTest\Entity\AbstractEntityTest;
+use Elcodi\Component\Tax\Entity\Tax;
+
+/**
+ * Class TaxTest.
+ */
+class TaxTest extends AbstractEntityTest
+{
+    /**
+     * Return the entity namespace.
+     *
+     * @return string Entity namespace
+     */
+    public function getEntityNamespace()
+    {
+        return 'Elcodi\Component\Tax\Entity\Tax';
+    }
+
+    /**
+     * Return the fields to test in entities.
+     *
+     * [
+     *      [
+     *          "type" => $this::GETTER_SETTER,
+     *          "getter" => "getValue",
+     *          "setter" => "setValue",
+     *          "value" => "Elcodi\Component\...\Interfaces\AnInterface"
+     *          "nullable" => true
+     *      ],
+     *      [
+     *          "type" => $this::ADDER_REMOVER|$this::ADDER_REMOVER,
+     *          "getter" => "getValue",
+     *          "setter" => "setValue",
+     *          "adder" => "addValue",
+     *          "removed" => "removeValue",
+     *          "nullable" => false,
+     *          "bag" => "collection", // can be array
+     *          "value" => "Elcodi\Component\...\Interfaces\AnInterface"
+     *      ]
+     * ]
+     *
+     * @return array Fields
+     */
+    public function getTestableFields()
+    {
+        return [
+            [
+                'type' => $this::GETTER_SETTER,
+                'getter' => 'getName',
+                'setter' => 'setName',
+                'value' => sha1(rand()),
+                'nullable' => false,
+            ],
+            [
+                'type' => $this::GETTER_SETTER,
+                'getter' => 'getDescription',
+                'setter' => 'setDescription',
+                'value' => sha1(rand()),
+                'nullable' => false,
+            ],
+            [
+                'type' => $this::GETTER_SETTER,
+                'getter' => 'getValue',
+                'setter' => 'setValue',
+                'value' => microtime(true),
+                'nullable' => false,
+            ],
+        ];
+    }
+
+    /**
+     * Test the string casting.
+     */
+    public function testToString()
+    {
+        $object = new Tax();
+        $name = sha1(rand());
+
+        $setterOutput = $object->setName($name);
+        $this->assertInstanceOf(get_class($object), $setterOutput);
+
+        $this->assertSame($name, (string) $object);
+    }
+}
