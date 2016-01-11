550395f900acf455355d4e1a374d1d5a5f8b1eb3
mmoreram <marc.morera@befactory.es>
2016-01-11 21:31:19 +0100

    Updated headers with new year

--- src/Elcodi/Component/Cart/EventListener/LoadCartPurchasablesQuantityEventListener.php
+++ src/Elcodi/Component/Cart/EventListener/LoadCartPurchasablesQuantityEventListener.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of the Elcodi package.
  *
- * Copyright (c) 2014-2015 Elcodi Networks S.L.
+ * Copyright (c) 2014-2016 Elcodi Networks S.L.
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
